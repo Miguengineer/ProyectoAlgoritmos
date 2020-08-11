b41177d52d03a49e5a1e7b8d6f8cad0a96610c03
krampus <durandiazmiguel@gmai.com>
2020-08-11 18:28:22 -0400

    Clases básicas listas de la estructura

--- PRQuadTree.hpp
+++ PRQuadTree.hpp
@@ -6,41 +6,53 @@
 #include "City.hpp"
 using namespace std;
 
-
 struct Point{
     float x;
     float y;
     Point(float x = 0, float y = 0): x(x), y(y){};
 };
 
-/**
- * Estructura que representa un nodo. Contiene posición (Point) y contenido, que es un puntero a clase City.
- */
-struct Node{
-    Point pos;
+class Node{
+private:
+    Point position;
     Node* NWNode;
     Node* NENode;
     Node* SWNode;
     Node* SENode;
     string color;
-    City* content;
-    Node(Point pos = Point(), Node* NWNode = nullptr, Node* NENode = nullptr, Node* SWNode = nullptr,
-         Node* SENode = nullptr, string color = "WHITE", City* data = nullptr): pos(pos), content(data),
-         NWNode(NWNode), NENode(NENode), SWNode(SWNode), SENode(SENode), color(std::move(color)) {};
+    City content;
+public:
+    Node(){
+        position = Point();
+        NWNode = nullptr;
+        NENode = nullptr;
+        SWNode = nullptr;
+        SENode = nullptr;
+        color = "WHITE";
+        content = City();
+    };
+    explicit Node(Point pos, Node* NW = nullptr, Node* NE = nullptr, Node* SW = nullptr,
+             Node* SE = nullptr, string col = "WHITE", City data = City()){
+        position = pos;
+        NWNode = NW;
+        NENode = NE;
+        SWNode = SW;
+        SENode = SE;
+        color = move(col);
+        content = move(data);
+    }
 };
 
 
 class PRQuadTree {
 private:
-    // Cada uno de los 4 children (cuadrantes)
-    Node rootNode;
-    float xMin, xMax, yMin, yMax;
+    int CAPACITY;
 public:
     // Constructores
-    PRQuadTree();
+    PRQuadTree() = default;
+    explicit PRQuadTree(int capacity):CAPACITY(capacity){};
     // Destructor
     ~PRQuadTree();
-    void subdivide();
     void deletePoint();
     int quantityOfPoints();
     float populationAtPoint();
